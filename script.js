9c11135f6c76cde70a87c8868ee4f1b152ec9356
outerLeitmotiv <olivier.mouttet@gmail.com>
2023-12-12 14:34:18 +0100

    test displaying house

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/spaceport_low-poly/scene.gltf');
+        model.setAttribute('gltf-model', './assets/house/scene.gltf');
         model.setAttribute('scale', '0.1 0.1 0.1');
 
         model.addEventListener('loaded', () => {
