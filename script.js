ad562985904ddfb4cfd9050d0c19ec5f7e05c118
outerLeitmotiv <olivier.mouttet@gmail.com>
2023-12-13 12:33:08 +0100

    trying values for gpsMinDistance

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
         {
             name: 'Spaceport',
             location: {
-                lat: 46.998369,
-                lng: 6.941686,
+                lat: 46.998346,
+                lng: 6.941704,
             }
         }
     ]
@@ -26,7 +26,7 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/dino.glb');
+        model.setAttribute('gltf-model', './assets/house/scene.gltf');
         model.setAttribute('scale', '1 1 1');
 
         model.addEventListener('loaded', () => {
